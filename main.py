1a557a6fa7741f67870c9c8963f6dee805fcf970
Liezl Puzon <liezl.200@gmail.com>
2014-08-11 09:47:45 -0700

    Move header around

--- main.py
+++ main.py
@@ -17,6 +17,7 @@
 import jinja2
 import os
 import webapp2
+import header
 from google.appengine.ext import ndb
 
 def dropItems():
@@ -73,19 +74,3 @@ app = webapp2.WSGIApplication([
 ], debug=True)
 
 
-header = '''
-<header class="navbar navbar-static-top bs-docs-nav" id="top" role="banner">
-    <div class="container">
-      <div class="navbar-header">
-        <a href="../" class="navbar-brand">What To Wear</a>
-      </div>
-      <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
-        <ul class="nav navbar-nav">
-          <li><a href="/createItemForm">New Item</a></li>
-          <li><a href="/viewItems">Closet</a></li>  
-          <li><a href="/about">About</a></li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-  '''
