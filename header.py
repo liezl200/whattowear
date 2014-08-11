5bc4ced9cc4a7793d2c019e21defa792958c3dd9
Liezl Puzon <liezl.200@gmail.com>
2014-08-11 15:33:14 -0700

    Changes to style - work in progress

--- header.py
+++ header.py
@@ -1,14 +1,14 @@
 header = '''
-<header class="navbar navbar-static-top bs-docs-nav" id="top" role="banner">
+<header class="navbar navbar-static-top bs-docs-nav" id="top" role="banner" style="background-color:rgba(2,132,130,0.7);">
     <div class="container">
       <div class="navbar-header">
-        <a href="../" class="navbar-brand">What To Wear</a>
+        <a style="color: white !important;" href="../" class="navbar-brand">What To Wear</a>
       </div>
       <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
-        <ul class="nav navbar-nav">
-          <li><a href="/createItemForm">New Item</a></li>
-          <li><a href="/viewItems">Closet</a></li>  
-          <li><a href="/about">About</a></li>
+        <ul class="nav navbar-nav" >
+          <li><a style="color: white !important;" href="/createItemForm">New Item</a></li>
+          <li><a style="color: white !important;" href="/viewItems">Closet</a></li>  
+          <li><a style="color: white !important;" href="/about">About</a></li>
         </ul>
       </nav>
     </div>
