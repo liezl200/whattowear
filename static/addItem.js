a3845972a6a646cc70afe278fbd7f0cab9c31036
Lukas <lukasmunoz255@gmail.com>
2014-08-12 18:16:15 -0700

    adding color wheel. Small changes to temporary.html

--- static/addItem.js
+++ static/addItem.js
@@ -1,5 +1,7 @@
 var colorBoxArray;
-var lastRGB = [-1, -1, -1, ''];
+var lastRGB = [-1, -1, -1];
+var lastBorderColor = 0;
+var colorWheel;
 
 function main()
 {
@@ -23,29 +25,9 @@ function initializeHandlers()
 
 function initializeColorSelector()
 {
-    colorBoxArray = [];
     var canvas = document.getElementById("colorSelector");
-    var ctx = canvas.getContext('2d');
-    var rgbArray = 
-    [[255, 0, 0, 'red'], //red
-    [255, 255, 0, 'yellow'], //yellow
-    [255, 170, 20, 'orange'], //orange
-    [150, 50, 50, 'dark purple'], //dark purple
-    [100, 0, 150, 'light purple'], //light purple
-    [200, 0, 255, 'brown'], //brown
-    [0, 155, 0, 'green'], //green
-    [190, 240, 255, 'baby blue'], //baby blue
-    [0, 0, 255, 'blue'], //blue
-    [0, 0, 155, 'navy blue'],
-    [0, 0, 0, 'black'], //navy blue
-    [254, 254, 254, 'white'],
-    [200, 200, 200, 'gray']];
-    var boxWidth = canvas.width / rgbArray.length;
-    for(var x = 0; x < rgbArray.length && x*boxWidth < canvas.width; x++)
-        colorBoxArray.push(new ColorBox(x*boxWidth, 0, boxWidth, canvas.height, rgbArray[x]));
-    for(var i = 0; i < colorBoxArray.length; i++)
-        colorBoxArray[i].draw(ctx);
-    canvas.onclick = colorOnClick;
+    colorWheel = Raphael.colorwheel(canvas, 200);
+    colorWheel.onchange(colorOnChange);
 }
 
 function removeWaterMark()
@@ -58,12 +40,20 @@ function removeWaterMark()
         if(imageData.data[i+3] < 1)
             continue;
         if(imageData.data[i] < 210 && imageData.data[i+1] < 210 && imageData.data[i+2] < 210)
-            continue;
-        if(imageData.data[i] == 255 && imageData.data[i+1] == 254 && imageData.data[i+2] == 252)
-            continue;
-        imageData.data[i] = 254;
-        imageData.data[i+1] = 254;
-        imageData.data[i+2] = 254;
+        {
+            imageData.data[i] = 0;
+            imageData.data[i+1] = 0;
+            imageData.data[i+2] = 0;
+        }
+        else
+        {
+            imageData.data[i] = 254;
+            imageData.data[i+1] = 254;
+            imageData.data[i+2] = 254;
+        }
+        //if(imageData.data[i] == 255 && imageData.data[i+1] == 254 && imageData.data[i+2] == 252)
+        //    continue;
+        
     }
     lastRGB[0] = 254;
     lastRGB[1] = 254;
@@ -71,16 +61,40 @@ function removeWaterMark()
     ctx.putImageData(imageData, 0, 0);
 }
 
-function changeColor(r, g, b, name)
+function changeColor(r, g, b)
 {
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext('2d');
     var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var changeBorder = false;
+    var light = false
+    if(r + g + b < 200 && lastBorderColor == 0)
+    {
+        changeBorder = true;
+        light = true;
+    }
+    else if(r + g + b >= 200 && lastBorderColor == 255)
+        changeBorder = true;
     for(var i = 0; i < imageData.data.length; i += 4)
     {
-        if(imageData.data[i] == 255 && imageData.data[i+1] == 254 && imageData.data[i+2] == 252)
+        if(imageData.data[i+3] < 1)
             continue;
-        if(imageData.data[i] == lastRGB[0] && imageData.data[i+1] == lastRGB[1] && imageData.data[i+2] == lastRGB[2])
+        if(changeBorder)
+        {
+            var newBorderColor = 0;
+            if(light)
+                newBorderColor = 255;
+            if(imageData.data[i] == lastBorderColor && imageData.data[i+1] == lastBorderColor 
+                && imageData.data[i+2] == lastBorderColor)
+            {
+                imageData.data[i] = newBorderColor;
+                imageData.data[i+1] = newBorderColor;
+                imageData.data[i+2] = newBorderColor;
+                lastBorderColor = newBorderColor;
+            }
+        }
+        if(imageData.data[i] != lastBorderColor && imageData.data[i+1] != lastBorderColor 
+            && imageData.data[i+2] != lastBorderColor)
         {
             imageData.data[i] = r;
             imageData.data[i+1] = g;
@@ -90,45 +104,16 @@ function changeColor(r, g, b, name)
     lastRGB[0] = r;
     lastRGB[1] = g;
     lastRGB[2] = b;
-    lastRGB[3] = name;
     ctx.putImageData(imageData, 0, 0);
 }
 
-function colorOnClick(mouse)
+function colorOnChange(raphaelRGB)
 {
-    var x = mouse.layerX;
-    var y = mouse.layerY;
-    for(var i = 0; i < colorBoxArray.length; i++)
-        colorBoxArray[i].onClick(x, y);
+    var r = raphaelRGB.r;
+    var g = raphaelRGB.g;
+    var b = raphaelRGB.b;
+    changeColor(r, g, b)
 }
-
-// Color Boc
-function ColorBox(x, y, width, height, rgb)
-{
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.r = rgb[0];
-    this.g = rgb[1];
-    this.b = rgb[2];
-    this.colorName = rgb[3];
-}
-
-ColorBox.prototype.onClick = function(x, y)
-{
-    if(x > this.x && x < this.x + this.width
-        && y > this.y && y < this.y + this.height)
-        changeColor(this.r, this.g, this.b, this.colorName);
-};
-
-ColorBox.prototype.draw = function(ctx)
-{
-    ctx.fillStyle = "rgb(" + this.r.toString() + "," + 
-        this.g.toString() + "," + this.b.toString() + ")";
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-};
-// End Color Selector
 
 function onClick()
 {
@@ -140,7 +125,7 @@ function onClick()
     imageId += topBottomString();
     var image = document.getElementById(imageId);
 
-    ctx.fillStyle = "rgb(255,254,252)";
+    ctx.fillStyle = "rgba(255,255,255,0)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(image, 0, 10, 190, 260);
     removeWaterMark();
@@ -149,8 +134,6 @@ function onClick()
 function onSubmit()
 {
     var hiddenColor = document.getElementById('hiddenColor');
-    var hiddenColorName = document.getElementById('hiddenColorName');
-    hiddenColorName.value = lastRGB[3];
     hiddenColor.value = toHex(lastRGB[0], lastRGB[1], lastRGB[2]);
 }
 
--- static/renderItems.js
+++ static/renderItems.js
@@ -3,6 +3,8 @@ function renderShirtsMain()
 	var container = document.getElementById("formContainer");
 	for(var i = 0; i < container.children.length; i += 2)
 	{
+		if(container.children[i].tagName === "BR")
+			i += 1;
 		var canvas = container.children[i];
 		var form = container.children[i+1];
 		var rgb = getRGB(form);
@@ -11,7 +13,6 @@ function renderShirtsMain()
 		var pattern = getPattern(form);
 		drawShirt(canvas, rgb[0], rgb[1], rgb[2], topOrBottom, longOrShort, pattern);
 	}
-
 }
 
 function getRGB(form)
@@ -19,7 +20,7 @@ function getRGB(form)
 	var hex = form.elements["hexValue"];
 	var r = hex.value.substr(0, 2);
 	var g = hex.value.substr(2, 2);
-	var b = hax.value.substr(4, 2);
+	var b = hex.value.substr(4, 2);
 	var rgbArray = [parseInt(r, 16), parseInt(g, 16), parseInt(b, 16)];
 	return rgbArray;
 }
@@ -46,8 +47,21 @@ function drawShirt(canvas, r, g, b, topOrBottom, longOrShort, pattern)
 {
 	var ctx = canvas.getContext('2d');
 	var imageId = "";
-	imageId += getLongOrShort();
-	imageId += getTopOrBottom();
+	imageId += topOrBottom;
+	imageId += longOrShort;
 	var image = document.getElementById(imageId);
-	ctx.drawImage(image, 0, 10, 95, 130);
+	ctx.drawImage(image, 0, 10, canvas.height, 130);
+	var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    for(var i = 0; i < imageData.data.length; i += 4)
+    {
+        if(imageData.data[i] == 255 && imageData.data[i+1] == 254 && imageData.data[i+2] == 252)
+            continue;
+        if(imageData.data[i] == lastRGB[0] && imageData.data[i+1] == lastRGB[1] && imageData.data[i+2] == lastRGB[2])
+        {
+            imageData.data[i] = r;
+            imageData.data[i+1] = g;
+            imageData.data[i+2] = b;
+        }
+    }
+    ctx.putImageData(imageData, 0, 0);
 }
