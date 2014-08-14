b3d6d5fd8fef5822c5f977c8c7cd34922f325661
Liezl Puzon <liezl.200@gmail.com>
2014-08-13 23:16:33 -0700

    Front end fixes

--- header.py
+++ header.py
@@ -45,7 +45,7 @@ footer = '''
       <iframe id="forecast_embed" type="text/html" frameborder="0" height="245" width="100%" src="http://forecast.io/embed/#lat=47.6097&lon=122.3331&name=Seattle&color=teal"> </iframe>
     </div>
   </div>
-  <div class="col-lg-4" style="background-color: white; z-index: 4; height: 245px;">
+  <div class="col-lg-4" style="background-color: white; z-index: 4; height: 288px;">
     <p style="color:grey;">
       </br>
       Hello, there!</br>
